def1a52806e29ba45e901a35e0fdf7e5bf94fda6
Hyeok-Cheon Kwon <snow1324@hcnc.co.kr>
2026-03-24 17:36:12 +0900

    Initial commit

---
+++ android/app/src/main/java/com/hcnc/memomemo/memomemo/MainActivity.java
@@ -0,0 +1,6 @@
+package com.hcnc.memomemo.memomemo;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'edit.dart';
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: ListView(
+        physics: BouncingScrollPhysics(),
+        children: <Widget>[
+          Container(color: Colors.redAccent, height: 70,),
+          Container(color: Colors.orange, height: 70,),
+          Container(color: Colors.yellow, height: 70,),
+          Container(color: Colors.green, height: 70,),
+          Container(color: Colors.blue, height: 70,),
+          Container(color: Colors.indigo, height: 70,),
+          Container(color: Colors.purpleAccent, height: 70,),
+        ]
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: (){
+          Navigator.push(
+            context,
+            CupertinoPageRoute(builder: (context) => EditPage()
+            ),
+          );
+        },
+        tooltip: '메모를 추가하려면 클릭하세요',
+        label: const Text('메모 추가'),
+        icon: const Icon(Icons.add),
+      ),
+    );
+  }
+}
